3e8dc7e43aea17ddfe3aaa419e1c6040491a90a0
Destiny Ezekiel <DestinyTch@proton.me>
2024-09-26 16:01:13 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,39 +62,25 @@ const scrollUp = () => {
 };
 window.addEventListener("scroll", scrollUp);
 /*=============== DARK LIGHT THEME ===============*/
-const themeButton = document.getElementById("theme-button");
-const darkTheme = "dark-theme";
-const iconTheme = "ri-sun-line";
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem("selected-theme");
-const selectedIcon = localStorage.getItem("selected-icon");
+const themeButton = document.querySelector('.ri-moon-clear-line');
+const body = document.body;
 
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () =>
-  document.body.classList.contains(darkTheme) ? "dark" : "light";
-const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
-
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
-    darkTheme
-  );
-  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
-    iconTheme
-  );
-}
-
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener("click", () => {
-  // Add or remove the dark / icon theme
-  document.body.classList.toggle(darkTheme);
-  themeButton.classList.toggle(iconTheme);
-  // We save the theme and the current icon that the user chose
-  localStorage.setItem("selected-theme", getCurrentTheme());
-  localStorage.setItem("selected-icon", getCurrentIcon());
+themeButton.addEventListener('click', () => {
+  body.classList.toggle('dark-theme');
+  
+  // Optionally, save the theme in localStorage to persist between page reloads
+  if (body.classList.contains('dark-theme')) {
+    localStorage.setItem('selected-theme', 'dark');
+  } else {
+    localStorage.setItem('selected-theme', 'light');
+  }
 });
+
+// On page load, check for the saved theme
+const savedTheme = localStorage.getItem('selected-theme');
+if (savedTheme === 'dark') {
+  body.classList.add('dark-theme');
+
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
   origin: "top",
